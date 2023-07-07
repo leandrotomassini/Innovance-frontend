f8cae535090601c168b6745ffce864cf9013ba06
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-07-07 15:02:08 -0300

    animation

--- src/app/dashboard/components/courses-taken/courses-taken.component.ts
+++ src/app/dashboard/components/courses-taken/courses-taken.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class CoursesTakenComponent {
 
 
-  urlImg = 'https://static.platzi.com/cdn-cgi/image/width=768,quality=85,format=auto/https://mdstrm.com/thumbs/512e13acaca1ebcd2f000279/thumb_5fecddef685ffb24261260a0_5fecddef685ffb24261260ad_51s.jpg';
+  urlImg: string[] = [
+    'https://static.platzi.com/cdn-cgi/image/width=768,quality=85,format=auto/https://mdstrm.com/thumbs/512e13acaca1ebcd2f000279/thumb_5fecddef685ffb24261260a0_5fecddef685ffb24261260ad_51s.jpg',
+    'https://static.platzi.com/cdn-cgi/image/width=768,quality=85,format=auto/https://thumbs.cdn.mdstrm.com/thumbs/512e13acaca1ebcd2f000279/thumb_64106bc48e6d8708cb846241_64106bc48e6d8708cb84624c_44s.jpg',
+    'https://static.platzi.com/cdn-cgi/image/width=768,quality=85,format=auto/https://mdstrm.com/thumbs/512e13acaca1ebcd2f000279/thumb_6008a32717f06b06ea1cbc9a_6008a32717f06b06ea1cbca6_93s.jpg'
+  ];
 }
