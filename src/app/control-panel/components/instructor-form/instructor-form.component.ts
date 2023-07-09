0f4426c9c25b6f1b99a73b5f31226516f8e9459d
Leandro C. Tomassini <leandroctomasini@gmail.com>
2023-07-09 18:07:26 -0300

    Instructores completo

--- src/app/control-panel/components/instructor-form/instructor-form.component.ts
+++ src/app/control-panel/components/instructor-form/instructor-form.component.ts
@@ -27,6 +27,7 @@ export class InstructorFormComponent implements OnInit {
     private fb: FormBuilder,
     private snackBar: MatSnackBar
   ) {
+    // Crear el formulario con sus controles y validaciones
     this.instructorForm = this.fb.group({
       imgUrl: ['', [Validators.required]],
       title: ['', [Validators.required]],
@@ -36,9 +37,11 @@ export class InstructorFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // Obtener la lista de usuarios
     this.authService.usersList().subscribe((users: User[]) => {
       this.users = users;
 
+      // Si se está editando un instructor, buscarlo por id y cargar sus datos en el formulario
       if (this.instructorId) {
         this.instructorService.findById(this.instructorId).subscribe((instructor: Instructor) => {
           this.instructor = instructor;
@@ -50,6 +53,7 @@ export class InstructorFormComponent implements OnInit {
           });
         });
       } else {
+        // Si se está creando un nuevo instructor, seleccionar al usuario actual como el creador del instructor
         const instructorUser = this.authService.currentUser?.id;
         if (instructorUser) {
           const instructorIndex = this.users.findIndex(user => user.id === instructorUser);
@@ -59,24 +63,28 @@ export class InstructorFormComponent implements OnInit {
             });
           }
         }
-        this.instructorForm.removeControl('status'); // Remover el control 'status' para un nuevo instructor
+        // Remover el control 'status' para un nuevo instructor
+        this.instructorForm.removeControl('status');
       }
     });
   }
 
   saveInstructor() {
+    // Si el formulario es inválido, no hacer nada
     if (this.instructorForm.invalid) {
       return;
     }
 
     const formValue = this.instructorForm.value;
 
+    // Crear un objeto con los datos del instructor a guardar
     let instructor: Partial<Instructor> = {
       imgUrl: formValue.imgUrl,
       title: formValue.title,
       user: formValue.user
     };
 
+    // Si se está editando un instructor, agregar el id y el estado al objeto instructor
     if (this.instructorId) {
       instructor = {
         ...instructor,
@@ -84,8 +92,10 @@ export class InstructorFormComponent implements OnInit {
         status: formValue.status === 'true'
       };
 
+      // Actualizar el instructor en la base de datos
       this.instructorService.updateById(this.instructorId, instructor as Instructor).subscribe({
         next: () => {
+          // Mostrar un mensaje de éxito y cerrar el diálogo
           this.snackBar.open('Instructor guardado', 'OK', {
             duration: 3000
           }).onAction().subscribe(() => {
@@ -95,14 +105,17 @@ export class InstructorFormComponent implements OnInit {
         },
         error: (error) => {
           console.error('Error al guardar el instructor:', error);
+          // Mostrar un mensaje de error
           this.snackBar.open('Error al guardar el instructor', 'OK', {
             duration: 3000
           });
         }
       });
     } else {
+      // Crear un nuevo instructor en la base de datos
       this.instructorService.create(instructor as Instructor).subscribe({
         next: () => {
+          // Mostrar un mensaje de éxito y cerrar el diálogo
           this.snackBar.open('Instructor guardado', 'OK', {
             duration: 3000
           }).onAction().subscribe(() => {
@@ -112,6 +125,7 @@ export class InstructorFormComponent implements OnInit {
         },
         error: (error) => {
           console.error('Error al guardar el instructor:', error);
+          // Mostrar un mensaje de error
           this.snackBar.open('Error al guardar el instructor', 'OK', {
             duration: 3000
           });
@@ -121,6 +135,7 @@ export class InstructorFormComponent implements OnInit {
   }
 
   closeModal(): void {
+    // Cerrar el diálogo sin guardar cambios
     this.dialogRef.close(false);
   }
 }
