e1bea5752ad4211744cc0774494a7b5eddea14ec
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-07-10 16:23:36 -0300

    control panel module

--- src/app/control-panel/components/instructors-table/instructors-table.component.ts
+++ src/app/control-panel/components/instructors-table/instructors-table.component.ts
@@ -1,66 +1,71 @@
-  import { Component, OnInit } from '@angular/core';
-  import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
-  import { InstructorService } from '../../services/instructor.service';
-  import { Instructor } from '../../interfaces';
-  import { InstructorFormComponent } from '../instructor-form/instructor-form.component';
+import { InstructorService } from '../../services';
+import { Instructor } from '../../interfaces';
+import { InstructorFormComponent } from '../instructor-form/instructor-form.component';
 
-  @Component({
-    selector: 'app-instructors-table',
-    templateUrl: './instructors-table.component.html',
-    styleUrls: ['./instructors-table.component.css']
-  })
-  export class InstructorsTableComponent implements OnInit {
-    instructorsList: Instructor[] = [];
-    filteredInstructors: Instructor[] = [];
-    filterValue: string = '';
+@Component({
+  selector: 'app-instructors-table',
+  templateUrl: './instructors-table.component.html',
+  styleUrls: ['./instructors-table.component.css']
+})
+export class InstructorsTableComponent implements OnInit {
+  instructorsList: Instructor[] = [];
+  filteredInstructors: Instructor[] = [];
+  filterValue: string = '';
 
-    columnLabels: { [key: string]: string } = {
-      imgUrl: 'Foto',
-      fullName: 'Nombre Completo',
-      title: 'Título'
-    };
+  columnLabels: { [key: string]: string } = {
+    imgUrl: 'Foto',
+    fullName: 'Nombre Completo',
+    title: 'Título'
+  };
 
-    displayedColumns: string[] = Object.keys(this.columnLabels);
+  displayedColumns: string[] = Object.keys(this.columnLabels);
 
-    constructor(
-      private instructorService: InstructorService,
-      private dialog: MatDialog
-    ) { }
+  constructor(
+    private instructorService: InstructorService,
+    private dialog: MatDialog
+  ) { }
 
-    ngOnInit(): void {
-      this.instructorService.findAll().subscribe((instructors: Instructor[]) => {
+  ngOnInit(): void {
+    this.instructorService.findAll()
+      .subscribe((instructors: Instructor[]) => {
         this.instructorsList = instructors;
         this.applyFilter();
       });
-    }
+  }
 
-    applyFilter(): void {
-      const filterText = this.filterValue.trim().toLowerCase();
-      if (filterText === '') {
-        this.filteredInstructors = this.instructorsList;
-      } else {
-        this.filteredInstructors = this.instructorsList.filter((instructor: Instructor) =>
+  applyFilter(): void {
+    const filterText = this.filterValue.trim().toLowerCase();
+    if (filterText === '') {
+      this.filteredInstructors = this.instructorsList;
+    } else {
+      this.filteredInstructors = this.instructorsList
+        .filter((instructor: Instructor) =>
           instructor.title.toLowerCase().includes(filterText)
         );
-      }
     }
+  }
 
-    openModal(instructorId: string): void {
-      const dialogRef = this.dialog.open(InstructorFormComponent, {
-        width: '400px'
-      });
+  openModal(instructorId: string): void {
+    const dialogRef = this.dialog.open(InstructorFormComponent, {
+      width: '400px'
+    });
 
-      dialogRef.componentInstance.instructorId = instructorId; // Asignar instructorId al componente hijo
+    // Asignar instructorId al componente hijo
+    dialogRef.componentInstance.instructorId = instructorId;
 
-      dialogRef.afterClosed().subscribe(result => {
-        if (result) {
-          // Si se guardó correctamente, actualizar la lista de instructores
-          this.instructorService.findAll().subscribe((instructors: Instructor[]) => {
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Si se guardó correctamente, actualizar la lista de instructores
+        this.instructorService.findAll()
+          .subscribe((instructors: Instructor[]) => {
             this.instructorsList = instructors;
             this.applyFilter();
           });
-        }
-      });
-    }
+      }
+    });
   }
+}
+
--- src/app/control-panel/control-panel.module.ts
+++ src/app/control-panel/control-panel.module.ts
@@ -1,38 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../material/material.module';
 import { ControlPanelRoutingModule } from './control-panel-routing.module';
-import { ControlPanelLayoutComponent } from './pages/control-panel-layout/control-panel-layout.component';
-import { UsersComponent } from './pages/users/users.component';
-import { UsersTableComponent } from './components/users-table/users-table.component';
-import { SubscriptionsComponent } from './pages/subscriptions/subscriptions.component';
-import { UsersFormComponent } from './components/users-form/users-form.component';
-import { SubscriptionsTableComponent } from './components/subscriptions-table/subscriptions-table.component';
-import { InstructorsComponent } from './pages/instructors/instructors.component';
-import { InstructorsTableComponent } from './components/instructors-table/instructors-table.component';
-import { InstructorFormComponent } from './components/instructor-form/instructor-form.component';
+import { ControlPanelComponentsModule } from './components/control-panel-components.module';
+import { PagesControlPanelModule } from './pages/pages-control-panel.module';
 
 
 @NgModule({
-  declarations: [
-    ControlPanelLayoutComponent,
-    UsersComponent,
-    UsersTableComponent,
-    SubscriptionsComponent,
-    UsersFormComponent,
-    SubscriptionsTableComponent,
-    InstructorsComponent,
-    InstructorsTableComponent,
-    InstructorFormComponent
-  ],
   imports: [
     CommonModule,
     ControlPanelRoutingModule,
     MaterialModule,
-    FormsModule,
-    ReactiveFormsModule
+    ControlPanelComponentsModule,
+    PagesControlPanelModule
   ]
 })
 export class ControlPanelModule { }
