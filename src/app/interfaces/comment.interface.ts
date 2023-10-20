385d8acf633956786c55898ff69c052492243f9e
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-10-20 16:04:49 -0300

    Comments menu

--- src/app/interfaces/comment.interface.ts
+++ src/app/interfaces/comment.interface.ts
@@ -1,6 +1,9 @@
+import { User } from "../auth/interfaces";
+
 export interface CommentVideo {
     idComment: string;
     status:    boolean;
     updatedAt: Date;
     comment:   string;
+    user: User;
 }
